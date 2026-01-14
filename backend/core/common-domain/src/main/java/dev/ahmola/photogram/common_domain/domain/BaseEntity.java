fb7f252e755f7b985f81b8d50da39d1ad850ca64
Jason <66013853+ahmola@users.noreply.github.com>
2026-01-14 17:50:06 +0900

    common domain fix #0

--- backend/core/common-domain/src/main/java/dev/ahmola/photogram/common_domain/domain/BaseEntity.java
+++ backend/core/common-domain/src/main/java/dev/ahmola/photogram/common_domain/domain/BaseEntity.java
@@ -3,14 +3,17 @@ package dev.ahmola.photogram.common_domain.domain;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 
 @MappedSuperclass
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
     @CreatedDate
     @Column(nullable = false, updatable = false)
